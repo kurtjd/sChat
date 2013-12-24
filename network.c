dce9d6e5ea38339f28b7ece4199f5970cca9c23c
mcconnellwade <mcconnellwade@gmail.com>
2013-12-25 00:35:32 +0800

    formatting

--- network.c
+++ network.c
@@ -13,37 +13,37 @@
 
 #include "network.h"
 
-void create_daemon( const int port ){
+void create_daemon(const int port){
 
-	// define and configure local and peer sockets 
+    // define and configure local and peer sockets 
 
-	int lsockfd, psockfd;
-	socklen_t sockaddr_size = sizeof( struct sockaddr );
+    int lsockfd, psockfd;
+    socklen_t sockaddr_size = sizeof(struct sockaddr);
 
-	struct sockaddr_in server;
-	struct sockaddr_in peer;
+    struct sockaddr_in server;
+    struct sockaddr_in peer;
 
-	if( (  lsockfd = socket( AF_INET, SOCK_STREAM, 0 ) )  < 0 ){
-		printf("Error: Unable to create socket\n");
-		exit(1);
-	}
+    if((lsockfd = socket( AF_INET, SOCK_STREAM, 0)) < 0){
+        printf("Error: Unable to create socket\n");
+        exit(1);
+    }
 
-	server.sin_family = AF_INET;
-	server.sin_port = htons( port );
-	server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = INADDR_ANY;
 
-	if( bind( lsockfd, (struct sockaddr *)&server, sockaddr_size ) < 0 ){
-		printf( "Error: Unable to bind to port %i\n", port );
-		exit(1);
-	}
+    if(bind(lsockfd, (struct sockaddr *)&server, sockaddr_size) < 0){
+        printf( "Error: Unable to bind to port %i\n", port );
+        exit(1);
+    }
 
-	if( listen( lsockfd, MAX_CONNECTIONS ) < 0 ){
-		printf( "Error: Unable to listen on port %i\n", port );
-		exit(1);
-	}
+    if(listen(lsockfd, MAX_CONNECTIONS ) < 0){
+        printf("Error: Unable to listen on port %i\n", port);
+        exit(1);
+    }
 
-	psockfd = accept( lsockfd, (struct sockaddr *)&peer, &sockaddr_size );
-	dup2( psockfd, 1 );
-	// printf("Connected\n");
-	close( psockfd );
+    psockfd = accept(lsockfd, (struct sockaddr *)&peer, &sockaddr_size);
+    dup2( psockfd, 1 );
+    // printf("Connected\n");
+    close(psockfd);
 }

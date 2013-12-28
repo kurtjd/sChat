dab292f5c4bece883350e469c8bc894fc3a31579
mcconnellwade <mcconnellwade@gmail.com>
2013-12-29 00:07:04 +0800

    peer connect

--- network.c
+++ network.c
@@ -81,3 +81,48 @@ int peer_listen(const char *port){
 
 	return 1;
 }
+
+int peer_connect(const char *host, const char *port){
+
+	int sockfd;
+	struct addrinfo client, *servinfo, *p;
+
+	memset(&client, 0, sizeof(client));
+	client.ai_family = AF_UNSPEC;
+	client.ai_socktype = SOCK_STREAM;
+
+	if((getaddrinfo(host, port, &client, &servinfo)) != 0){
+		fprintf(stderr, "Unable to assemble address info\n");
+		return 0;
+	}
+
+	// Loop through our struct and connect to the first possible result
+
+	while(1){
+
+		for(p = servinfo; p != NULL; p = p->ai_next) {
+
+			if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))  < 0 ){
+				fprintf(stderr, "Error unable to create socket\n");
+				continue;
+			}
+
+			printf("Connecting to peer %s:%s\n",host,port);
+
+			if(connect(sockfd, p->ai_addr, p->ai_addrlen) < 0){
+				fprintf(stderr, "Error: Connect\n");
+				close(sockfd);
+				continue;
+			}
+
+			break;
+		}
+
+		if(p) break;
+	}
+
+	printf("Connected to peer %s:%s\n",host,port);
+	freeaddrinfo(servinfo);
+
+	return 1;
+}
--- network.h
+++ network.h
@@ -6,5 +6,6 @@
 #define MAX_CONNECTIONS 2
 
 int peer_listen(const char *port);
+int peer_connect(const char *host, const char *port);
 
 #endif

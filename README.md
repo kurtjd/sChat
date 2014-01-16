f0464b675c87fc37b2920befc7b83f149b734c8c
Kurtis Dinelle <kurtisdinelle@gmail.com>
2014-01-15 23:51:50 -0700

    Added license for tfw official looking.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 sChat
 =====
-sChat: A light-weight, encrypted peer-to-peer chat program.
+A light-weight, encrypted peer-to-peer chat program.
+
+Authors
+=======
+Kurtis Dinelle (kurtisdinelle@gmail.com)
+McConnell Wade (mcconnellwade@gmail.com)
--- txtfield.c
+++ txtfield.c
@@ -1,3 +1,22 @@
+/******************************************************************************
+ * txtfield.c: Implementation of the text field container.                    *
+ * Copyright (C) 2014  Kurtis Dinelle & McConnell Wade                        *
+ *                                                                            *
+ * This file is a part of sChat.                                              *
+ *                                                                            *
+ * sChat is free software: you can redistribute it and/or modify              *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * sChat is distributed in the hope that it will be useful,                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with sChat.  If not, see [http://www.gnu.org/licenses/].             *
+ ******************************************************************************/
 #include <stdlib.h>
 #include <string.h>
 #include "xcurses.h"
